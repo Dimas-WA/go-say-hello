3f1806c50f5c86440bcbab70229e0da875858387
Dimas-WA <109815005+Dimas-WA@users.noreply.github.com>
2024-06-26 12:06:50 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Dimas-WA/go-say-hello
+module github.com/Dimas-WA/go-say-hello/v2
 
 go 1.22.4
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_max
 
-func SayHello() string {
-	return "Hello"
+func SayHello(name string) string {
+	return "Hello " + name
 }
